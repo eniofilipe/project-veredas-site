70ed63b2a3ec2986afcb521f14a540fe19e437f9
Enio Filipe <eniofms@protonmail.com>
2020-12-13 18:58:10 -0300

    create cart context

---
+++ src/contexts/cart.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+import { Oferta } from '../types';
+
+interface OfertaCart extends Oferta {
+  quantidadeCart: number;
+}
+
+interface ICartContext {
+  products: OfertaCart[];
+  addProduct: (prod: Oferta) => void;
+}
+
+const CartContext = createContext<ICartContext>({} as ICartContext);
+
+export const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<OfertaCart[]>([]);
+
+  const addProduct = (prod: Oferta) => {
+    const index = products.findIndex((value) => value.id === prod.id);
+
+    console.log(index);
+
+    if (index !== -1) {
+      const productsAux = products;
+
+      productsAux[index] = {
+        ...productsAux[index],
+        quantidadeCart: productsAux[index].quantidadeCart + 1,
+      };
+
+      setProducts(productsAux);
+    } else {
+      const productAux = { ...prod, quantidadeCart: 1 } as OfertaCart;
+
+      setProducts(products.concat(productAux));
+    }
+  };
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContext;
