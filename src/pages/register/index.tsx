60b8cd61c902843f1224d83e27ae85d077af6b30
jkevinx23 <kevingomesr@gmail.com>
2020-12-13 14:28:54 -0300

    Pagina registro responsiva

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,88 +1,149 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable operator-linebreak */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Head from 'next/head';
+import { useState } from 'react';
+import * as S from './styles';
+import veredaslogo from '../../assets/logo.png';
 
-import {
-  Header,
-  Container,
-  RegisterContainer,
-  InfoContainer,
-  AddressContainer,
-  InputContainer,
-  InputForm,
-  ButtonsContainer,
-  ButtonLogin,
-  FinallyButton,
-} from './styles';
+export type ProfileProps = {
+  name: string;
+  cpf: string;
+  phone: string;
+  email: string;
+  cep: string;
+  neighborhood: string;
+  street: string;
+  number: string;
+  complement?: string;
+  ref?: string;
+};
 
-const register = () => (
-  <div>
-    <body>
-      <Header>Cadastro</Header>
-      <Container>
-        <RegisterContainer>
-          <InfoContainer>
-            <span>Dados</span>
-            <InputContainer>
-              <span>Nome:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>CPF:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Telefone:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>E-mail:</span>
-              <InputForm type="email" />
-            </InputContainer>
-            <InputContainer>
-              <span>Senha:</span>
-              <InputForm type="password" />
-            </InputContainer>
-            <InputContainer>
-              <span>Confirmar senha:</span>
-              <InputForm type="password" />
-            </InputContainer>
-          </InfoContainer>
-          <AddressContainer>
-            <span>Endereço</span>
-            <InputContainer>
-              <span>CEP:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Bairro:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Rua:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Número:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Compl:</span>
-              <InputForm type="text" />
-            </InputContainer>
-            <InputContainer>
-              <span>Referência:</span>
-              <InputForm type="text" />
-            </InputContainer>
-          </AddressContainer>
-        </RegisterContainer>
-        <ButtonsContainer>
-          <ButtonLogin clear onClick={() => {}}>
-            Já tenho uma conta
-          </ButtonLogin>
-          <FinallyButton onClick={() => {}}>Finalizar</FinallyButton>
-        </ButtonsContainer>
-      </Container>
-    </body>
-  </div>
-);
+const Profile = () => {
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-export default register;
+  const [cep, setCep] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [street, setStreet] = useState('');
+  const [number, setNumber] = useState('');
+  const [complement, setComplement] = useState('');
+  const [reference, setReference] = useState('');
+
+  return (
+    <S.Wrapper>
+      <Head>
+        <title>Veredas da terra</title>
+      </Head>
+      <S.Header>
+        <S.Logo src={veredaslogo} alt="" />
+        <S.Title>Cadastro</S.Title>
+      </S.Header>
+      <S.Body>
+        <S.WrapperController>
+          <S.WrapperContent>
+            <S.WrapperData>
+              <S.Title>Dados</S.Title>
+              <S.Row>
+                <S.Label> Nome: </S.Label>
+                <S.Input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> CPF: </S.Label>
+                <S.Input value={cpf} onChange={(e) => setCpf(e.target.value)} />
+              </S.Row>
+              <S.Row>
+                <S.Label> Telefone: </S.Label>
+                <S.Input
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> E-Mail: </S.Label>
+                <S.Input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </S.Row>
+
+              <S.Row>
+                <S.Label> Senha: </S.Label>
+                <S.Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </S.Row>
+
+              <S.Row>
+                <S.Label> Confirmar Senha: </S.Label>
+                <S.Input
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  type="password"
+                />
+              </S.Row>
+            </S.WrapperData>
+            <S.WrapperAddress>
+              <S.Title>Endereço</S.Title>
+              <S.Row>
+                <S.Label> CEP: </S.Label>
+                <S.Input value={cep} onChange={(e) => setCep(e.target.value)} />
+              </S.Row>
+              <S.Row>
+                <S.Label> Bairro: </S.Label>
+                <S.Input
+                  value={neighborhood}
+                  onChange={(e) => setNeighborhood(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> Rua: </S.Label>
+                <S.Input
+                  value={street}
+                  onChange={(e) => setStreet(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> Número: </S.Label>
+                <S.Input
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> Compl.: </S.Label>
+                <S.Input
+                  value={complement}
+                  onChange={(e) => setComplement(e.target.value)}
+                />
+              </S.Row>
+              <S.Row>
+                <S.Label> Referência: </S.Label>
+                <S.Input
+                  value={reference}
+                  onChange={(e) => setReference(e.target.value)}
+                />
+              </S.Row>
+            </S.WrapperAddress>
+          </S.WrapperContent>
+          <S.WrapperButtons>
+            <S.Button>Já tenho conta</S.Button>
+            <S.Button>Finalizar</S.Button>
+          </S.WrapperButtons>
+        </S.WrapperController>
+      </S.Body>
+    </S.Wrapper>
+  );
+};
+
+export default Profile;
--- src/pages/register/styles.tsx
+++ src/pages/register/styles.tsx
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-import Input from '../../components/Inputs/Input';
-import Button from '../../components/Buttons/Button';
+export const Logo = styled.img`
+  width: 16rem;
+  position: absolute;
+  left: 2rem;
+  top: 0.4rem;
+`;
+
+export const Title = styled.p`
+  font-size: 2rem;
+  color: rgba(95, 30, 31, 0.7);
+  cursor: default;
+`;
 
 export const Header = styled.div`
   display: flex;
@@ -10,95 +20,249 @@ export const Header = styled.div`
   background: rgba(30, 149, 31, 0.4);
   justify-content: center;
   align-items: center;
-  padding: 5%;
-  height: 126px;
-`;
+  padding: 2rem 1%;
+  ${Title} {
+    color: #444;
+  }
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
+  @media only screen and (max-width: 695px) {
+    ${Logo} {
+      width: 14rem;
+      left: 1rem;
+    }
+  }
 
-export const RegisterContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-self: center;
-  max-width: 1100px;
+  @media only screen and (max-width: 600px) {
+    padding: 1.2rem 1%;
 
-  width: 100%;
-  @media (max-width: 600px) {
-    flex-direction: column;
+    ${Logo} {
+      width: 10rem;
+      left: 1rem;
+    }
+    ${Title} {
+      font-size: 2rem;
+    }
+  }
+
+  @media only screen and (max-width: 467px) {
+    padding: 1.2rem 1%;
+
+    ${Logo} {
+      width: 8rem;
+      left: 0.7rem;
+    }
+    ${Title} {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media only screen and (max-width: 367px) {
+    padding: 1rem 1%;
+
+    ${Logo} {
+      width: 6rem;
+      left: 0.5rem;
+    }
+    ${Title} {
+      font-size: 1.2rem;
+    }
+  }
+  @media only screen and (max-width: 275px) {
+    justify-content: flex-end;
+
+    padding: 1rem 1%;
+
+    ${Logo} {
+      width: 7rem;
+      left: 0.5rem;
+    }
+    ${Title} {
+      font-size: 1.2rem;
+      padding-right: 1rem;
+    }
   }
 `;
 
-export const InfoContainer = styled.div`
+export const Button = styled.button`
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+  border: none;
+  height: 3rem;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    color: rgba(95, 30, 31, 0.7);
+    border: 1px solid rgba(95, 30, 31, 0.7);
+  }
+`;
+
+export const Wrapper = styled.div``;
+export const WrapperContent = styled.div`
   display: flex;
-  flex-direction: column;
-  border: 1px solid ${(props) => props.theme.colors.secondary};
-  border-radius: 10px;
-  padding: 10px;
+  gap: 2rem;
+`;
+export const Label = styled.p`
+  font-size: 1.4rem;
+  color: rgba(95, 30, 31, 0.7);
+`;
+
+export const Input = styled.input`
+  margin-left: auto;
   width: 60%;
-  margin: 10px;
-
-  @media (max-width: 600px) {
-    width: auto;
-  }
+  border-radius: 4px;
+  border: 1px solid #444;
+  outline: none;
+  font-size: 1.4rem;
+  color: rgba(95, 30, 31, 0.7);
+  padding-top: 2px;
+  padding-bottom: 2px;
+  padding-left: 4px;
 `;
 
-export const AddressContainer = styled.div`
+export const WrapperData = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid ${(props) => props.theme.colors.secondary};
-  border-radius: 10px;
-  padding: 10px;
-  margin: 10px;
-  width: 40%;
-  @media (max-width: 600px) {
-    width: auto;
+  border: 1px solid #444;
+  padding: 1rem 2rem;
+  border-radius: 4px;
+  row-gap: 1rem;
+  height: 100%;
+  ${Button} {
+    margin-left: auto;
+    margin-top: 1rem;
+    width: 25%;
+    margin-bottom: 0.5rem;
   }
 `;
 
-export const InputContainer = styled.div`
+export const WrapperAddress = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin: 5px 0;
+  flex-direction: column;
+  border: 1px solid #444;
+  padding: 1rem 2rem;
+  border-radius: 4px;
+  row-gap: 1rem;
+  height: 100%;
 `;
-export const InputForm = styled(Input)`
-  width: 80%;
-  height: 35px;
+export const WrapperController = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 4rem;
+
+  @media only screen and (max-width: 1393px) {
+    width: 80%;
+  }
+  @media only screen and (max-width: 1217px) {
+    width: 90%;
+  }
+
+  @media only screen and (max-width: 1083px) {
+    ${Label} {
+      font-size: 1.5rem;
+    }
+    ${Input} {
+      width: 50%;
+    }
+  }
+
+  @media only screen and (max-width: 901px) {
+    ${Label} {
+      font-size: 1.2rem;
+    }
+    ${Input} {
+      width: 45%;
+    }
+  }
+
+  @media only screen and (max-width: 750px) {
+    padding-top: 2rem;
+
+    ${WrapperData} {
+      padding: 0.5rem 1rem;
+    }
+    ${WrapperAddress} {
+      padding: 0.5rem 1rem;
+    }
+  }
+  @media only screen and (max-width: 675px) {
+    width: 80%;
+    ${WrapperContent} {
+      flex-direction: column;
+    }
+    ${Label} {
+      font-size: 1.5rem;
+    }
+    ${Input} {
+      width: 50%;
+    }
+  }
+
+  @media only screen and (max-width: 505px) {
+    width: 85%;
+
+    ${Label} {
+      font-size: 1.3rem;
+    }
+    ${Input} {
+      width: 45%;
+    }
+  }
+
+  @media only screen and (max-width: 400px) {
+    width: 90%;
+
+    ${Label} {
+      font-size: 1.2rem;
+    }
+    ${Input} {
+      width: 40%;
+    }
+  }
 `;
 
-export const ButtonsContainer = styled.div`
+export const Body = styled.body`
+  background-color: rgba(196, 196, 196, 0.1);
+  height: 110vh;
+  max-width: 1580px;
+  margin: 0 auto;
+`;
+
+export const WrapperMenu = styled.div`
   display: flex;
-  flex-direction: row;
+  position: absolute;
+  right: 1rem;
+  top: 2rem;
+  gap: 3rem;
+  ${Title} {
+    cursor: pointer;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const WrapperButtons = styled.div`
+  display: flex;
   justify-content: space-between;
-  margin: 10px 20px;
-  max-width: 1100px;
-  align-self: center;
   width: 100%;
-  padding: 10px;
-  @media (max-width: 600px) {
-    flex-direction: column;
-  }
-`;
+  margin: 1rem auto;
 
-export const ButtonLogin = styled(Button)`
-  height: 50px;
-  padding: 0 20px;
-  @media (max-width: 600px) {
-    width: 75%;
-    margin: 10px;
+  ${Button} {
+    width: 6rem;
   }
-`;
-
-export const FinallyButton = styled(Button)`
-  height: 50px;
-  padding: 0 20px;
-  @media (max-width: 600px) {
-    width: 75%;
-    margin: 10px;
+  ${Button}:nth-child(2) {
+    background: green;
+    color: white;
+    border: 1px solid green;
+    &:hover {
+      color: green;
+      background: white;
+    }
   }
 `;
