0bc25bcf1a4bb4d5439eefd4a64e1c4e2ec23f8b
Enio Filipe <eniofms@protonmail.com>
2020-12-15 16:31:05 -0300

    fix bugs to deploy

--- src/api/Clientes.ts
+++ src/api/Clientes.ts
@@ -1,6 +1,9 @@
 import api from './Api';
-import { PostClienteProps } from '../types';
+import { PostClienteProps, Cliente } from '../types';
 
 export const postClientes = async (data: PostClienteProps) => api.post('/cliente', data);
+
+export const getDetails = async (id: number) =>
+  api.get<Cliente>(`/cliente/id/${id}`);
 
 export default { postClientes };
