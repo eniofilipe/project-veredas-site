72297a8d3117796c3bcab49ea98ded128ace2ca6
jefesonk1 <jefesonk1@outlook.com>
2021-04-19 00:42:05 -0300

    adicionado query limite na rota get /pedido para puxar mais de 20 pedidos(padrao)

--- src/api/Pedidos.ts
+++ src/api/Pedidos.ts
@@ -4,6 +4,11 @@ import { PostPedidoProps, PedidosProps } from '../types';
 export const postPedido = async (data: PostPedidoProps) =>
   api.post('/pedido', data);
 
-export const getPedidos = async () => api.get<PedidosProps[]>('/pedido');
+export const getPedidos = async () => api.get<PedidosProps[]>('/pedido',{
+  params: {
+    pagina: '1',
+    limite: '2147483640',
+  },
+});
 
 export default { getPedidos, postPedido };
